dde7b51e289ca4828ad6ecfae0f059ea4c001b25
eveilleo <leo.eveille@univ-rouen.fr>
2024-02-21 17:29:33 +0100

    changement

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,13 @@
-# Utilisez l'image Python officielle comme base
 FROM python:3.8-slim
 
-# Définissez le répertoire de travail dans le conteneur
 WORKDIR /app
 
-# Copiez les fichiers nécessaires dans le conteneur
 COPY requirements.txt .
 COPY app.py .
 COPY templates/ templates/
 
-# Installez les dépendances
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Exposez le port sur lequel l'application Flask s'exécute
 EXPOSE 5000
 
-# Commande pour démarrer l'application Flask
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -10,16 +10,31 @@ def home():
 @app.route('/newuser/', methods=['GET', 'POST'])
 def new_user():
     if request.method == 'POST':
-        utilisateur = request.form['utilisateur']
+        username = request.form['username']
+        password = request.form['password']
+        mail = request.form['mail']
  
-        if re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$", utilisateur):
-            message = "L'identifiant respecte les critères spécifiés."
+        # Critères regex pour l'identifiant
+        if re.match(r"^[a-z]{6,10}$", username):
+            message_username = "L'identifiant respecte les critères spécifiés."
         else:
-            message = "L'identifiant ne respecte pas les critères spécifiés."
+            message_username = "L'identifiant ne respecte pas les critères spécifiés."
 
-        return render_template('newuser.html', message=message)
+        # Critères regex pour le mot de passe
+        if re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[%#{}@]).{6,}$", password):
+            message_password = "Le mot de passe respecte les critères spécifiés."
+        else:
+            message_password = "Le mot de passe ne respecte pas les critères spécifiés."
 
-    return render_template('newuser.html', message=None)
+        # Critères regex pour l'adresse email
+        if re.match(r'^[A-Za-z0-9_\-\.]+@[A-Za-z0-9\-\.]+$', mail):
+            message_mail = "L'adresse email est valide."
+        else:
+            message_mail = "L'adresse email n'est pas valide."
+
+        return render_template('newuser.html', message_username=message_username, message_password=message_password, message_mail=message_mail)
+
+    return render_template('newuser.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
